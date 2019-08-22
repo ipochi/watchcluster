adec9f4c59c9853006de1d8484bd63be758f00c9
Imran Pochi <pochiimran@yahoo.co.in>
2019-08-23 00:43:58 +0530

    removing commented code

--- main.go
+++ main.go
@@ -19,34 +19,4 @@ func main() {
 	}
 
 	watchcluster.Start(watchclusterConfig)
-	//Start(watchclusterConfig)
-
-	// 	log.Logger.Info("Starting controller")
-	// Config, err := config.New()
-	// if err != nil {
-	// 	log.Logger.Fatal(fmt.Sprintf("Error in loading configuration. Error:%s", err.Error()))
-	// }
-
-	// if Config.Communications.Slack.Enabled {
-	// 	log.Logger.Info("Starting slack bot")
-	// 	sb := bot.NewSlackBot()
-	// 	go sb.Start()
-	// }
-
-	// if Config.Communications.Mattermost.Enabled {
-	// 	log.Logger.Info("Starting mattermost bot")
-	// 	mb := bot.NewMattermostBot()
-	// 	go mb.Start()
-	// }
-
-	// if Config.Settings.UpgradeNotifier {
-	// 	log.Logger.Info("Starting upgrade notifier")
-	// 	go controller.UpgradeNotifier(Config)
-
-	// }
-
-	// 		// Init KubeClient, InformerMap and start controller
-	// 		utils.InitKubeClient()
-	// 	utils.InitInformerMap()
-	// 	controller.RegisterInformers(Config)
 }
